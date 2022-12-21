cd47f2e9e2d5cce6a931ec60d833ba3656ea4eb1
Arya Karimi <devaryakrm@gmail.com>
2022-12-21 11:33:29 +0300

    store added

--- src/state/store.js
+++ src/state/store.js
@@ -1,16 +1,95 @@
+import { nanoid } from 'nanoid';
 import create from 'zustand';
 
-const useStore = create((set) => ({
-    count: 1,
-    inc: () => set((state) => ({ count: state.count + 1 })),
+const INIT_HOUSES = [
+    {
+        id: nanoid(),
+        name: '',
+        price: 1000,
+        location: '',
+        rate: 5,
+        images: [],
+        timeStamp: Date.now(),
+        startDate: '',
+        endDate: '',
+        owner: {
+            id: nanoid(),
+            name: '',
+            avatar: '',
+        },
+        details: {
+            guests: 0,
+            bedrooms: 0,
+            beds: 0,
+            baths: 0,
+        },
+        offers: {
+            wifi: false,
+            kitchen: false,
+            parking: false,
+            pets: false,
+            tv: false,
+            pool: false,
+            smoke: false,
+        },
+        reviews: [
+            {
+                id: nanoid(),
+                avatar: '',
+                user: '',
+                content: '',
+            },
+        ],
+        isEdited: false,
+    },
+];
+
+// price convert
+// const convertPrice = (priceInUSD, option)  => {
+//     const CURRENCIES = {
+//         TUR: 18.5,
+//         RON: 4.6,
+//     }
+
+//     return CURRENCIES[option] * priceInUSD
+// }
+
+const useStoreHouses = create((set) => ({
+    houses: INIT_HOUSES,
+    addHouse: (newHouse) => {
+        return set((state) => ({
+            houses: [...state.houses, newHouse],
+        }));
+    },
+    deleteHouse: (houseId) => {
+        return set((state) => ({
+            houses: state.houses.filter(({ id }) => id !== houseId),
+        }));
+    },
+    isEdited: (houseId) => {
+        return set((state) => ({
+            houses: state.houses.map((house) =>
+                house.id === houseId
+                    ? { ...house, isEdited: !house.isEdited }
+                    : house
+            ),
+        }));
+    },
+    editHouse: (houseId, editedHouse) => {
+        return set((state) => ({
+            houses: state.houses.map((house) =>
+                house.id === houseId ? { ...house, ...editedHouse } : house
+            ),
+        }));
+    },
 }));
 
-function Controls() {
-    const inc = useStore((state) => state.inc);
-    return <button onClick={inc}>one up</button>;
-}
+// function Controls() {
+//     const addHouse = useStore((state) => state.addHouse);
+//     return <button onClick={() => addHouse(newHouse)}>Add House</button>;
+// }
 
-function Counter() {
-    const count = useStore((state) => state.count);
-    return <h1>{count}</h1>;
-}
+// function Counter() {
+//     const count = useStore((state) => state.count);
+//     return <h1>{count}</h1>;
+// }
