20f190e726643a5d2ccebd16cff5134b1317f7ab
Arya Karimi <devaryakrm@gmail.com>
2022-12-21 10:45:21 +0300

    Material UI added

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,6 +1,6 @@
-import Home from './Home';
-import SignIn from './SignIn';
-import SignUp from './SignUp';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 const AppRoutes = [
     {
